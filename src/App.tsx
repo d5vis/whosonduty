a2a077348ad8caf2c8e130299db8e4478075c757
d5vis <davisjbanks@gmail.com>
2023-09-08 23:37:05 -0700

    implement select for mis amigos

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import { useState, useEffect } from "react";
+
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Typography from "@mui/material/Typography";
 
 function App() {
+  const [date, setDate] = useState(new Date());
+  const [building, setBuilding] = useState("DRS");
+  const [ras, setRas] = useState(["none"]);
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setBuilding(event.target.value as string);
+  };
+
+  useEffect(() => {
+    setDate(new Date());
+    const url = `http://d5vis.pythonanywhere.com/`;
+    fetch(url + building)
+      .then((r) => r.json())
+      .then((r) => setRas(r))
+      .catch((e) => console.log(e));
+  }, [building]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App">
+      <Card sx={{ m: "8px" }} variant="outlined">
+        <FormControl sx={{ m: "8px" }}>
+          <InputLabel id="building-select-label">Building</InputLabel>
+          <Select
+            labelId="building-select-label"
+            id="building-select"
+            value={building}
+            label="Building"
+            onChange={handleChange}
+          >
+            <MenuItem value={"DRS"}>Del Rey South</MenuItem>
+            <MenuItem value={"DES"}>Desmond</MenuItem>
+            <MenuItem value={"PNHDOH"}>Palm North/Doheny</MenuItem>
+            <MenuItem value={"L56"}>Leavey 5/6</MenuItem>
+          </Select>
+        </FormControl>
+        <Typography> RA(s) on duty for {date.toLocaleDateString()} </Typography>
+        {ras.map((ra) => (
+          <Typography>{ra}</Typography>
+        ))}
+      </Card>
+    </Box>
   );
 }
 

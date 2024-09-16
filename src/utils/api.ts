f31688391535a45d9a68281f472708c9ced2a1c7
d5vis <davisjbanks@gmail.com>
2024-09-15 21:34:00 -0700

    update api to serve over https

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-const url = "http://d5vis.pythonanywhere.com/building/";
+const url = "https://d5vis.pythonanywhere.com/building/";
 
 export const fetchRas = async (building: string) => {
   return fetch(url + building).then((response) => response.json());
